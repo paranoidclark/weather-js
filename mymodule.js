c0c4f2650d7cedb5f45d1c2220455081359e25d7
Clark Kem Louie Gabato Paran <cklgparan@addunet.ph>
2024-04-15 19:53:25 +0800

    Added routing of portfolio

---
+++ mymodule.js
@@ -0,0 +1,11 @@
+const fulldata = [
+    {
+        first_name: 'Clark',
+        age: 22,
+        course: "Interdisciplinary Studies Minor in Media and Technology",
+        occupation: 'Student',
+        school: 'Ateneo de Davao University'
+    },
+]
+
+module.exports = { fulldata }
--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 var weather = require('weather-js');
 const express = require('express')
+const { fulldata } = require('./mymodule')
 const app = express()
+app.set('view engine', 'ejs')
 
 const port = 8000;
 
+app.use((req, res, next) => {
+    console.log("Request Made");
+    console.log(`Host: ${req.hostname}`);
+    console.log(`Path: ${req.path}`);
+    next()
+})
+
 app.use(express.static('public'));
-app.set('view engine', 'ejs')
+app.use(express.static('views'))
 
 app.get('/', function (req, res) {
+    res.render('index', { myData: fulldata })
+})
 
+app.get('/about', function (req, res) {
+    res.render('about', { myData: fulldata });
+})
+
+app.get('/portfolio', function (req, res) {
+    res.render('portfolio', { myData: fulldata })
+})
+
+app.get('/project', function (req, res) {
+    res.render('project', { myData: fulldata })
+})
+
+app.get('/index', (req, res) => {
+    res.redirect('/')
+})
+
+app.get('/weather', function (req, res) {
     const cities = ['San Francisco, CA', 'Davao City, Philippines', 'Paris, France']
     const weatherData = []
     const temperature = []
@@ -25,7 +53,7 @@ app.get('/', function (req, res) {
                 temperature.push(result[0].current.temperature);
                 listOfCities.push(result[0].location.name);
                 if (weatherData.length === cities.length) {
-                    res.render('index', { temperature: temperature, city: listOfCities, weatherData: weatherData });
+                    res.render('weather', { temperature: temperature, city: listOfCities, weatherData: weatherData });
                 }
             }
         });
@@ -35,4 +63,9 @@ app.get('/', function (req, res) {
         getWeather(city);
     })
 });
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/error.html', { root: __dirname })
+})
+
 app.listen(port, () => { console.log(`Server connected to port ${port}`) })
